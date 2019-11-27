eaf6f4f7ed3802fb1bfd86730dc1fba1a0ae42fc
Ryan Silva <RyandotSilva@gmail.com>
2019-11-27 17:55:13 -0500

    Add getPosts method

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -3,11 +3,15 @@ import { listPosts } from "../graphql/queries";
 import { API, graphqlOperation } from "aws-amplify";
 
 export default class DisplayPosts extends Component {
-
-  componentDidMount = aynsc () => {
+  componentDidMount = async () => {
     this.getPosts();
-  }
-  
+  };
+
+  getPosts = async () => {
+    const result = await API.graphql(graphqlOperation(listPosts));
+    console.log(`All Posts: JSON.stringify(${result.data.listPosts.items})`);
+  };
+
   render() {
     return <div>Display Posts</div>;
   }
