6cbe2c534a2c89cb25d2293bbadcb3e354b4dfff
Ryan Silva <RyandotSilva@gmail.com>
2019-11-27 17:49:39 -0500

    Add componentDidMount method

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -3,6 +3,11 @@ import { listPosts } from "../graphql/queries";
 import { API, graphqlOperation } from "aws-amplify";
 
 export default class DisplayPosts extends Component {
+
+  componentDidMount = aynsc () => {
+    this.getPosts();
+  }
+  
   render() {
     return <div>Display Posts</div>;
   }
