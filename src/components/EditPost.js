8054fd19b93e8ca397c959be5bc19e250669000d
Ryan Silva <RyandotSilva@gmail.com>
2019-12-02 10:14:14 -0500

    Add initial state

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class EditPost extends Component {
+  state = {
+    show: false,
+    id: "",
+    postOwnerId: "",
+    postOwnerUsername: "",
+    powerTitle: "",
+    postBody: ""
+  };
+
   render() {
     return <button>Edit</button>;
   }
