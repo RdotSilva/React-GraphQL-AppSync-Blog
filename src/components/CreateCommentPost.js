01f7ace631d797c3bb84b26d78f7d441930bf7a9
Ryan Silva <RyandotSilva@gmail.com>
2019-12-03 09:32:15 -0500

    Add initial state

--- src/components/CreateCommentPost.js
+++ src/components/CreateCommentPost.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class CreateCommentPost extends Component {
+  state = {
+    commentOwnerId: "",
+    commentOwnerUsername: "",
+    content: ""
+  };
+
   render() {
     return <div></div>;
   }
