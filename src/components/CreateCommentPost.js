941614865fc1667006f07294d960c30a18edd849
Ryan Silva <RyandotSilva@gmail.com>
2019-12-03 09:33:56 -0500

    Add componentDidMount

--- src/components/CreateCommentPost.js
+++ src/components/CreateCommentPost.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { Auth } from "aws-amplify";
 
 class CreateCommentPost extends Component {
   state = {
     commentOwnerId: "",
     commentOwnerUsername: "",
     content: ""
+  };
+
+  componentDidMount = async () => {
+    await Auth.currentUserInfo().then(user => {
+      this.setState({
+        commentOwnerId: user.attributes.sub,
+        commentOwnerUsername: user.username
+      });
+    });
   };
 
   render() {
