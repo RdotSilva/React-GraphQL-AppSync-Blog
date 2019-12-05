e3742fa30933b93cb27ab5da2e2e341bf908179c
RdotSilva <RyandotSilva@gmail.com>
2019-12-05 10:15:45 -0500

    Add alert for user if liking own post

--- src/components/DisplayPosts.js
+++ src/components/DisplayPosts.js
@@ -140,18 +140,24 @@ class DisplayPosts extends Component {
   };
 
   handleLike = async postId => {
-    const input = {
-      numberLikes: 1,
-      likeOwnerId: this.state.ownerId,
-      likeOwnerUsername: this.state.ownerUsername,
-      likePostId: postId
-    };
+    if (this.likedPost(postId)) {
+      return this.setState({ errorMessage: "Can't like your own post." });
+    } else {
+      const input = {
+        numberLikes: 1,
+        likeOwnerId: this.state.ownerId,
+        likeOwnerUsername: this.state.ownerUsername,
+        likePostId: postId
+      };
 
-    try {
-      const result = await API.graphql(graphqlOperation(createLike, { input }));
-      console.log("Liked: ", result.data);
-    } catch (error) {
-      console.error(error);
+      try {
+        const result = await API.graphql(
+          graphqlOperation(createLike, { input })
+        );
+        console.log("Liked: ", result.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -178,6 +184,9 @@ class DisplayPosts extends Component {
             {post.postOwnerId === loggedInUser && <DeletePost data={post} />}
             {post.postOwnerId === loggedInUser && <EditPost {...post} />}
             <span>
+              <p className="alert">
+                {post.postOwnerId === loggedInUser && this.state.errorMessage}
+              </p>
               <p onClick={() => this.handleLike(post.id)}>
                 <FaThumbsUp />
                 {post.likes.items.length}
