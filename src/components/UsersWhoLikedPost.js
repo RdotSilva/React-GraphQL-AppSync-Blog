6dac7572affe65d28c883ac76360419fdc21345e
RdotSilva <RyandotSilva@gmail.com>
2019-12-05 11:34:51 -0500

    Add UsersWhoLikedPost component

---
+++ src/components/UsersWhoLikedPost.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class UsersWhoLikedPost extends Component {
+  render() {
+    const allUsers = this.props.data;
+    return allUsers.map(user => {
+      return (
+        <span key={user}>
+          <span style={{ fontStyle: "bold", color: "#ged" }}>{user}</span>
+        </span>
+      );
+    });
+  }
+}
+
+export default UsersWhoLikedPost;
